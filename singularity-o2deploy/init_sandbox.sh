e4da7f132a6049647ec1610f4df25cfa2e46a9a5
Jeremy Wilkinson <jeremy.wilkinson@cern.ch>
2024-03-20 16:44:22 +0100

    bump o2dev/o2deploy to alma9

--- singularity-o2deploy/init_sandbox.sh
+++ singularity-o2deploy/init_sandbox.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-singularity build  --force --docker-login --fix-perms --fakeroot --sandbox sandboxo2/  definition_o2sandboxcontainer_slc7.def
+singularity build  --force --docker-login --fix-perms --fakeroot --sandbox sandboxo2/  definition_o2sandboxcontainer.def
 
